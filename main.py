50ae0db66662de7504650d5b1b26c6bcc36f22e9
Luis Gabriel Neves Tristao <luisgabrielnevest@gmail.com>
2026-08-26 14:28:17 -0300

    Adiciona listagem numerada, remoção de tarefas por índice e prepara persistência com json

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
+import json
+
 tarefas = []
 while True:
     print("\n------Menu----")
     print("1 - Adicionar tarefa")
     print("2 - Listar tarefas")
-    print("3 - Sair")
+    print("3 - Remover Tarefa")
+    print("Qualquer outro para sair!")
     resp = input("Escolha 1 opcao ai: ")
     if resp == "1":
         nometarefas = input("Digite o nome da tarerfa: ")
         tarefas.append(nometarefas)
 
     elif resp == "2":
-        print(tarefas)
+        if len(tarefas) == 0:
+            print("Nenhuma tarefa adicionada")
+        else:
+            for indice, tarefa in enumerate(tarefas):
+                print(indice + 1, tarefa)
+
+    elif resp == "3":
+        tirar = int(input("Qual quer remover?(Digite o numero dela) "))
+        tarefas.pop(tirar-1)
 
     else:
         print("Saindo...")
